415892c9a6402e7c5acd3554d99062c3c44daefe
Psinary <Psinary@163.com>
2023-07-01 18:08:48 +0800

    Update v2note_update.py

--- v2note_update.py
+++ v2note_update.py
@@ -39,33 +39,16 @@ def run(playwright: Playwright,password,save_path) -> None:
 
     # Click button:has-text("确定")
     page.locator("button:has-text(\"确定\")").click()
-
-    # Click div[role="button"]:has-text("10")
-    page.locator("div[role=\"button\"]:has-text(\"10\")").click()
-
-    # Click text=全部
-    page.locator("text=全部").click()
-
-    # Click [aria-label="速度\: ：未排序。 点击以升序排列。"] i
-    page.locator("[aria-label=\"速度\\: ：未排序。 点击以升序排列。\"] i").click()
-
-    # Click [aria-label="速度\: ：升序排列。 点击以降序排列。"] i
-    page.locator("[aria-label=\"速度\\: ：升序排列。 点击以降序排列。\"] i").click()
-
-    # Click .v-input--selection-controls__input > .v-icon >> nth=0
-    page.locator(".v-input--selection-controls__input > .v-icon").first.click()
-
-    # Click button:has-text("转换")
+    page.locator("text=全选").first.click()
+    page.locator("text=速度").click()
+    page.locator("text=速度").click()
     page.locator("button:has-text(\"转换\")").click()
+    # Click div[role="menuitem"]:has-text("Clash")
+    page.locator("div[role=\"menuitem\"]:has-text(\"Clash\")").click()
 
-    # Click div[role="menu"] >> text=Clash
-    page.locator("div[role=\"menu\"] >> text=Clash").click()
-
-    # Click button:has-text("下载")
     with page.expect_download() as download_info:
         page.locator("button:has-text(\"下载\")").click()
     download = download_info.value
-
 
     download_res = [download.path(), download.url, download.suggested_filename, save_path]
 
